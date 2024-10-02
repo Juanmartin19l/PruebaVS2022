cfb27deccf21c86c38b948b5676ae0468e7c318b
Juan Martin Lavalle <lavallejuanmartin@gmail.com>
2024-10-02 16:14:52 -0300

    Agregar archivos de proyecto.

---
+++ Store/Controllers/brandsController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Store;
+
+namespace Store.Controllers
+{
+    public class brandsController : Controller
+    {
+        private Database1Entities db = new Database1Entities();
+
+        // GET: brands
+        public ActionResult Index()
+        {
+            return View(db.brands.ToList());
+        }
+
+        // GET: brands/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            brands brands = db.brands.Find(id);
+            if (brands == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brands);
+        }
+
+        // GET: brands/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: brands/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "brand_id,brand_name")] brands brands)
+        {
+            if (ModelState.IsValid)
+            {
+                db.brands.Add(brands);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(brands);
+        }
+
+        // GET: brands/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            brands brands = db.brands.Find(id);
+            if (brands == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brands);
+        }
+
+        // POST: brands/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "brand_id,brand_name")] brands brands)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(brands).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(brands);
+        }
+
+        // GET: brands/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            brands brands = db.brands.Find(id);
+            if (brands == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brands);
+        }
+
+        // POST: brands/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            brands brands = db.brands.Find(id);
+            db.brands.Remove(brands);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Search(string query)
+        {
+            var results = db.brands
+                            .Where(b => b.brand_name.Contains(query) || string.IsNullOrEmpty(query))
+                            .ToList();
+
+            return PartialView("_BrandsTable", results);
+        }
+
+        public ActionResult Sort(string column, string direction)
+        {
+            IQueryable<brands> brands = db.brands;
+
+            // Ordenar según la columna seleccionada
+            switch (column)
+            {
+                case "brand_name":
+                    brands = direction == "asc" ? brands.OrderBy(b => b.brand_name) : brands.OrderByDescending(b => b.brand_name);
+                    break;
+                default:
+                    brands = direction == "asc" ? brands.OrderBy(b => b.brand_id) : brands.OrderByDescending(b => b.brand_id);
+                    break;
+            }
+
+            // Retornar los resultados ordenados como una vista parcial para AJAX
+            return PartialView("_BrandsTable", brands.ToList());
+        }
+    }
+}
---
+++ Store/Controllers/categoriesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Store;
+
+namespace Store.Controllers
+{
+    public class categoriesController : Controller
+    {
+        private Database1Entities db = new Database1Entities();
+
+        // GET: categories
+        public ActionResult Index()
+        {
+            return View(db.categories.ToList());
+        }
+
+        // GET: categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            categories category = db.categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // GET: categories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: categories/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "category_id,category_name")] categories category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        // GET: categories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            categories category = db.categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: categories/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "category_id,category_name")] categories category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: categories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            categories category = db.categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            categories category = db.categories.Find(id);
+            db.categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // Método para buscar categorías
+        public ActionResult Search(string query)
+        {
+            // Filtrar las categorías cuyo nombre contenga el texto de búsqueda
+            var results = db.categories
+                            .Where(c => c.category_name.Contains(query) || string.IsNullOrEmpty(query))
+                            .ToList();
+
+            // Devolver una vista parcial con los resultados filtrados
+            return PartialView("_CategoriesTable", results);
+        }
+
+        // Método para ordenar categorías
+        public ActionResult Sort(string column, string direction)
+        {
+            IQueryable<categories> categories = db.categories;
+
+            // Ordenar según la columna y dirección
+            switch (column.ToLower())
+            {
+                case "category_name":
+                    categories = direction.ToLower() == "asc" ?
+                        categories.OrderBy(c => c.category_name) :
+                        categories.OrderByDescending(c => c.category_name);
+                    break;
+                default:
+                    categories = categories.OrderBy(c => c.category_id); // Por defecto, ordenar por ID
+                    break;
+            }
+
+            return PartialView("_CategoriesTable", categories.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
